735b8546e8058f52ce6d7cecd02718f21da88ab5
Rafael Veiga <dsg.rafael@gmail.com>
2022-07-14 22:43:15 -0300

    feat: add default apollo-server boilerplate

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,36 @@
+import { ApolloServer } from "apollo-server";
+import typeDefs from "./typeDefs";
 import db from "./firestore";
 
 class App {
   public async start() {
-    const data = await db.collection("micros").get();
+    const books = [
+      {
+        title: "The Awakening",
+        author: "Kate Chopin",
+      },
+      {
+        title: "City of Glass",
+        author: "Paul Auster",
+      },
+    ];
 
-    data.forEach((doc) => {
-      console.log(doc.id, "=>", doc.data());
+    const resolvers = {
+      Query: {
+        books: () => books,
+      },
+    };
+
+    const server = new ApolloServer({
+      typeDefs,
+      resolvers,
+      csrfPrevention: true,
+      cache: "bounded",
+    });
+
+    // The `listen` method launches a web server.
+    server.listen().then(({ url }) => {
+      console.log(`🚀  Server ready at ${url}`);
     });
   }
 }
